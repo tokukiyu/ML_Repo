ebca4be04cb7dd90aa2ce415630473486649d14d
tokukiyu <tokkiyuam@gmail.com>
2023-08-04 00:38:57 +0300

    update try

--- nlp_week_1_iii/pattern4.py
+++ nlp_week_1_iii/pattern4.py
@@ -2,7 +2,7 @@ import re
 numbers = '315-897-4873'
 pattern4 = r'[123]'
 pattern5 = r'[1-3]'
-pattern6 = r'[^1-3]'
+pattern6 = r'[^1-5]'
 patterns456 = [pattern4, pattern5, pattern6]
 for pattern in patterns456:
     if re.findall(pattern, numbers):
