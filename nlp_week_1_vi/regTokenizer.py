d49051298176156f87470ba22d123a95e051cc26
tokukiyu <tokkiyuam@gmail.com>
2023-08-06 09:17:32 +0300

    upd

--- nlp_week_1_vi/regTokenizer.py
+++ nlp_week_1_vi/regTokenizer.py
@@ -2,7 +2,7 @@ from nltk.tokenize import RegexpTokenizer
 
 white_space_pattern = RegexpTokenizer('\s+', gaps = True)
 #  ####
-codio = 'Codio is the best computer science? learning tool.'
+codio = 'Codio is the best computer science ? learning tool.'
 codio_tokenized = white_space_pattern.tokenize(codio)
 print(f"\nOriginal Output of 'codio': {codio}")
 print(f"\nRegexp Tokenizer Tokenized Output of 'codio': {codio_tokenized}")
