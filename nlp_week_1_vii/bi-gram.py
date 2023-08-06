21a3e35b061a444393dfe923f3ec4af011548716
tokukiyu <tokkiyuam@gmail.com>
2023-08-06 10:11:33 +0300

    nlp n-gram

---
+++ nlp_week_1_vii/bi-gram.py
@@ -0,0 +1,18 @@
+from nltk import UnigramTagger, BigramTagger, DefaultTagger, TrigramTagger
+from nltk.corpus import brown
+
+brown_tagged_sents = brown.tagged_sents(categories='news')
+brown_sents = brown.sents(categories='news')
+
+brown_tagged_sents_size = int(len(brown_tagged_sents) * 0.9)
+training_brown_tagged_sents = brown_tagged_sents[:brown_tagged_sents_size]
+testing_brown_tagged_sents = brown_tagged_sents[brown_tagged_sents_size:]
+
+brown_bigram_tagger = BigramTagger(training_brown_tagged_sents)
+print(brown_bigram_tagger.tag(brown_sents[2009]))
+print(brown_bigram_tagger.accuracy(testing_brown_tagged_sents))
+
+
+brown_additional_sentences = brown_sents[4205]
+print(brown_bigram_tagger.tag(brown_additional_sentences))
+print(brown_bigram_tagger.accuracy(testing_brown_tagged_sents))
