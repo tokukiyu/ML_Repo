5d0ba779070f9783b32f910b900a5c1e44f6365c
tokukiyu <tokkiyuam@gmail.com>
2023-08-06 10:14:22 +0300

    nlp n-gram

--- nlp_week_1_vii/bi-gram.py
+++ nlp_week_1_vii/bi-gram.py
@@ -16,3 +16,11 @@ print(brown_bigram_tagger.accuracy(testing_brown_tagged_sents))
 brown_additional_sentences = brown_sents[4205]
 print(brown_bigram_tagger.tag(brown_additional_sentences))
 print(brown_bigram_tagger.accuracy(testing_brown_tagged_sents))
+
+
+default_tagger = DefaultTagger('NN')
+tagger1 = UnigramTagger(training_brown_tagged_sents, backoff = default_tagger)
+tagger2 = BigramTagger(training_brown_tagged_sents, backoff = tagger1)
+tagger3 = TrigramTagger(training_brown_tagged_sents, backoff = tagger2)
+
+print(tagger3.accuracy(testing_brown_tagged_sents))
