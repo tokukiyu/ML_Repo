a59f77161f8781024c6310ff366907822a5ffce1
tokukiyu <tokkiyuam@gmail.com>
2023-08-06 10:05:10 +0300

    nlp n-gram

--- nlp_week_1_vii/uni-gram.py
+++ nlp_week_1_vii/uni-gram.py
@@ -9,3 +9,9 @@ brown_unigram_tagger = UnigramTagger(brown_tagged_sents)
 print(brown_unigram_tagger.tag(brown_sents[2009]))
 
 print(brown_unigram_tagger.accuracy(brown_tagged_sents))
+
+brown_tagged_sents_size = int(len(brown_tagged_sents) * 0.9)
+training_brown_tagged_sents = brown_tagged_sents[:brown_tagged_sents_size]
+testing_brown_tagged_sents = brown_tagged_sents[brown_tagged_sents_size:]
+brown_unigram_tagger2 = UnigramTagger(training_brown_tagged_sents)
+print(brown_unigram_tagger2.accuracy(testing_brown_tagged_sents))
